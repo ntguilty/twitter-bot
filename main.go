13eb0eb07510e0d816dab66e322540744b822cd3
ntguilty <budnicki.piotr@gmail.com>
2020-04-11 21:59:52 +0200

    added tokens for authentication against Twitter REST API

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/ChimeraCoder/anaconda"
+	"net/url"
+	"os"
 )
 
 //Tokens struct stores our access tokens and secret keys needed for
@@ -15,12 +16,20 @@ type Tokens struct {
 }
 
 func main() {
-	anaconda.SetConsumerKey(apiKey)
-	anaconda.SetConsumerSecret(apiSecretKey)
-	api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
-	res, _ := api.GetSearch("golang", nil)
-	for _ , tweet := range res.Statuses {
-		fmt.Print(tweet.Text)
+	tokens := Tokens{
+		ConsumerKey:       os.Getenv("TWITTERBOT_CONSUMER_KEY"),
+		ConsumerSecretKey: os.Getenv("TWITTERBOT_CONSUMER_SECRETKEY"),
+		TokenKey:       os.Getenv("TWITTERBOT_TOKEN_KEY"),
+		TokenSecretKey:    os.Getenv("TWITTERBOT_TOKEN_SECRETKEY"),
 	}
+	anaconda.SetConsumerKey(tokens.ConsumerKey)
+	anaconda.SetConsumerSecret(tokens.ConsumerSecretKey)
+	api := anaconda.NewTwitterApi(tokens.TokenKey, tokens.TokenSecretKey)
+
+	stream := api.PublicStreamFilter(url.Values{
+		"": []string{"#ntguiltydevbot-test", "@ntguiltydevbot"},
+	})
+
+	defer stream.Stop()
 
 }
